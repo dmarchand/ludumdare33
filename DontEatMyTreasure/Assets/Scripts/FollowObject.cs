6c094d3b34555a053ae570fd96fdaa373220b760
dmarchand <dan.marchand@gmail.com>
2015-08-22 13:56:23 -0400

    Fixed some scaling. Added player input + camera follow

---
+++ DontEatMyTreasure/Assets/Scripts/FollowObject.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowObject : MonoBehaviour {
+
+    public float DampTime = 0.15f;
+    public Transform Target;
+    private Vector3 velocity = Vector3.zero;
+    private Camera camera;
+
+    void Start()
+    {
+        camera = GetComponent<Camera>();
+    }
+
+	// Update is called once per frame
+	void Update () {
+        if (Target) {
+            
+            Vector3 point = camera.WorldToViewportPoint(Target.position);
+            Vector3 delta = Target.position - camera.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, point.z));
+            Vector3 destination = transform.position + delta;
+            transform.position = destination;//Vector3.SmoothDamp(transform.position, destination, ref velocity, DampTime);
+        }
+	}
+}
---
+++ DontEatMyTreasure/Assets/Scripts/MoveOnAxisInput.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveOnAxisInput : MonoBehaviour {
+
+    public string horizontalAxis = "Horizontal";
+    public string verticalAxis = "Vertical";
+    public float speed = 1.0f;
+
+    
+
+    void FixedUpdate()
+    {
+        transform.position += (Vector3.right * Input.GetAxis(horizontalAxis) + Vector3.up * Input.GetAxis(verticalAxis)).normalized * speed * Time.deltaTime;
+    }
+}
