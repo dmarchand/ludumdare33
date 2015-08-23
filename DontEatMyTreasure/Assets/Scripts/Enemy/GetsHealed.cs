1634ff70f5aae36156ca5d335146ab056e03c0ff
dmarchand <dan.marchand@gmail.com>
2015-08-22 23:36:50 -0400

    Priests heal other monsters!

---
+++ DontEatMyTreasure/Assets/Scripts/Enemy/GetsHealed.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GetsHealed : MonoBehaviour {
+
+    public Color BlinkColor;
+
+
+    public void Heal(int amount)
+    {
+        var model = GetComponent<EnemyModel>();
+        model.CurrentHP += amount;
+        model.CurrentHP = Mathf.Min(model.CurrentHP, model.MaxHP);
+
+        StartCoroutine(FlashUtils.BlinkSmooth(GetComponent<SpriteRenderer>(), 10, 2, BlinkColor));
+    }
+}
---
+++ DontEatMyTreasure/Assets/Scripts/Enemy/HealsAllies.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HealsAllies : MonoBehaviour {
+
+    public float HealDelay = 10f;
+    public int Power = 2;
+
+	// Use this for initialization
+	void Start () {
+        Invoke("Heal", HealDelay);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+    void Heal()
+    {
+        var allies = GameObject.FindGameObjectsWithTag("Monster");
+        foreach (var ally in allies)
+        {
+            if (ally.gameObject != gameObject)
+            {
+                ally.GetComponent<GetsHealed>().Heal(Power);
+            }
+        }
+
+        Invoke("Heal", HealDelay);
+    }
+}
